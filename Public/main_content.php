ae38d1ede057377bc2a865f80560133b4b3f689a
Mirko965 <mirkojelic.jelic@gmail.com>
2016-04-18 14:32:07 +0200

    Dodati include u main content

--- Public/main_content.php
+++ Public/main_content.php
@@ -1,19 +1,8 @@
-<!doctype html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Main Content</title>
-    <link rel="stylesheet" href="css/style.css">
-  </head>
+<?php include("../include/layout/header.php"); ?>
   <body>
 
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script src="js/main.js"></script>
   </body>
-  <footer>
-  </footer>
-</html>
+<?php include("../include/layout/footer.php"); ?>
