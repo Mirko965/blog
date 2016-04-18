2ab6a1094ef9477245c1c8b8fe0be6f93e127b15
Mirko965 <mirkojelic.jelic@gmail.com>
2016-04-18 15:18:20 +0200

    otklonjene greske

--- Public/main_content.php
+++ Public/main_content.php
@@ -1,9 +1,9 @@
 <?php include("../include/layout/header.php"); ?>
 <?php
-	define("DB_SERVER", "localhost ");
-	define("DB_USER", "mirkojelic ");
-	define("DB_PASS", "fionfion00 ");
-	define("DB_NAME", "my_blog ");
+	define("DB_SERVER", "mirkohost");
+	define("DB_USER", "mirkojelic");
+	define("DB_PASS", "fionfion00");
+	define("DB_NAME", "my_blog");
 
   // 1. Create a database connection
   $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
