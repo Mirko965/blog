ce2d3129754422dffa119351f21ff48da024737f
Mirko965 <mirkojelic.jelic@gmail.com>
2016-04-18 15:03:18 +0200

    u assets dodat pocetak konekcije i kraj konekcije

--- assets.php
+++ assets.php
@@ -1,3 +1,28 @@
+//CONECTION
+<?php
+	define("DB_SERVER", "localhost ");
+	define("DB_USER", "mirkojelic ");
+	define("DB_PASS", "fionfion00 ");
+	define("DB_NAME", "my_blog ");
+
+  // 1. Create a database connection
+  $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+  // Test if connection succeeded
+  if(mysqli_connect_errno()) {
+    die("Database connection failed: " .
+         mysqli_connect_error() .
+         " (" . mysqli_connect_errno() . ")"
+    );
+  }
+?>
+<?php
+// 5. CLOSE CONECTION
+if(isset($dbconn)){
+  mysqli_close($dbconn);
+}
+
+?>
+
 <?php
 //2.PERFORM DATABASES QUERY
 $query_select_author  = "SELECT * " ;
