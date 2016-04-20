bb72504f695ee0e88cc5e8744a8e0aa8876593fd
Mirko965 <mirkojelic.jelic@gmail.com>
2016-04-20 20:12:17 +0200

    Dodati clanci autora u nav

--- Public/main_content.php
+++ Public/main_content.php
@@ -16,17 +16,6 @@
     );
   }
 ?>
-<?php
-//2.PERFORM DATABASES QUERY
-$query  = "SELECT * " ;
-$query .= "FROM writer ";
-$query .= "WHERE visible = 1 ";
-$query .= "ORDER BY position ASC";
-$result = mysqli_query($dbconn, $query);
-if(!$result){
-  die("Database query failed");
-}
-?>
   <body>
     <header>
       <h1>My Blog</h1>
@@ -38,17 +27,55 @@ if(!$result){
       <aside>
         <nav>
           <ul class="writer">
-          <?php
-          while($writer = mysqli_fetch_assoc($result)){
-          ?>
-            <li><?php echo $writer["writer_name"] ?></li>
-          <?php
-          }
-          ?>
+            <?php
+             //2.PERFORM DATABASES QUERY
+             $query  = "SELECT * " ;
+             $query .= "FROM writer ";
+             $query .= "WHERE visible = 1 ";
+             $query .= "ORDER BY position ASC";
+             $result = mysqli_query($dbconn, $query);
+             if(!$result){
+               die("Database query failed");
+             }
+             ?>
+              <?php
+              while($writer = mysqli_fetch_assoc($result)){
+              ?>
+               <li><?php echo $writer["writer_name"] ?>
+                <ul class="text">
+                  <?php
+                  $query_text  = "SELECT * " ;
+                  $query_text .= "FROM text " ;
+                  $query_text .= "WHERE visible = 1 " ;
+                  $query_text .= "AND writer_id = {$writer["id"]} " ;
+                  $query_text .= "ORDER BY position ASC";
+                  $result_text = mysqli_query($dbconn, $query_text);
+                  if(!$result_text){
+                   die("Database query failed");
+                  }
+                  ?>
+                  <?php
+                  while($text = mysqli_fetch_assoc($result_text)){
+                  ?>
+                 <li>
+                   <?php echo $text["headline"] ?>
+                 </li>
+                  <?php
+                   }
+                  ?>
+                   <?php
+                   mysqli_free_result($result_text);
+                   ?>
+                </ul>
+              </li>
+            <?php
+            }
+            ?>
+            <?php
+            mysqli_free_result($result);
+            ?>
+          </ul>
         </nav>
       </aside>
     </article>
-<?php
-mysqli_free_result($result);
-?>
 <?php include("../include/layout/footer.php"); ?>
