74a7eeb11e67aed90167a9bcb597246d630c311e
Mirko965 <mirkojelic.jelic@gmail.com>
2016-04-26 12:44:44 +0200

    sve izbrisano

--- Public/main_content.php
+++ Public/main_content.php
@@ -2,42 +2,17 @@
 <?php require_once("../include/function.php"); ?>
 <?php include("../include/layout/header.php"); ?>
 
-<?php
-if(isset($_GET["writer"])){
-  $current_writer = find_writer_by_id($_GET["writer"]);
-  $current_text = null;
-} elseif(isset($_GET["text"])) {
-  $current_text = find_text_by_id( $_GET["text"]);
-  $current_writer = null;
-} else {
-  $current_text = null;
-  $current_writer = null;
-}
-?>
   <body>
     <header>
       <h1>My Blog</h1>
     </header>
     <article>
       <section class="main">
-              <?php if($current_writer){ ?>
-        <h2>Menage Writer</h2>
-              <p>Writer Name:</p>
-               <?php   echo $current_writer["writer_name"]; ?>
 
-              <?php } elseif( $current_text){ ?>
-              <h2>Menage Text</h2>
-              <p>Text Headline:</p>
-              <?php   echo $current_text["headline"]; ?>
-              <?php } else { ?>
-              <p>Please selected writer or text </p>
-              <?php } ?>
       </section>
       <aside>
         <nav>
-        <?php
-         echo navigation($current_writer["id"],$current_text["id"]);
-        ?>
+
         </nav>
       </aside>
     </article>
