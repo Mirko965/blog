c60fc416de8828415cc8f759726e236d44a3b8bc
Mirko965 <mirkojelic.jelic@gmail.com>
2016-04-26 11:24:41 +0200

    Posle funkcije za prikaz texta na glavnoj strani,

--- Public/main_content.php
+++ Public/main_content.php
@@ -20,15 +20,20 @@ if(isset($_GET["writer"])){
     </header>
     <article>
       <section class="main">
-        <h2>Menage Content</h2>
-              <?php if($selected_writer_id){               ?>
+              <?php if($selected_writer_id){ ?>
+        <h2>Menage Writer</h2>
               <?php $current_writer = find_writer_by_id($selected_writer_id);?>
-Writer Name:  <?php   echo $current_writer["writer_name"];            ?>
-              <?php } elseif( $selected_text_id){          ?>
-              <?php   echo $selected_text_id;              ?>
-              <?php } else {                               ?>
-<p>Please selected writer or text </p>
-              <?php }                                      ?>
+              <p>Writer Name:</p>
+               <?php   echo $current_writer["writer_name"]; ?>
+
+              <?php } elseif( $selected_text_id){ ?>
+              <?php $current_text = find_text_by_id($selected_text_id);?>
+              <h2>Menage Text</h2>
+              <p>Text Headline:</p>
+              <?php   echo $current_text["headline"]; ?>
+              <?php } else { ?>
+              <p>Please selected writer or text </p>
+              <?php } ?>
       </section>
       <aside>
         <nav>
