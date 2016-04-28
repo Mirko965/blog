8c8bdebfbb0e7be6e9158d73d91df9a841d4b010
Mirko965 <mirkojelic.jelic@gmail.com>
2016-04-28 20:12:01 +0200

    create db con, perform query, close db conn

--- Public/main_content.php
+++ Public/main_content.php
@@ -1,6 +1,18 @@
 <?php require_once("../include/db_connection.php"); ?>
 <?php require_once("../include/function.php"); ?>
 <?php include("../include/layout/header.php"); ?>
+<?php
+
+//2.PERFORM DATABASES QUERY
+$query  = "SELECT * " ;
+$query .= "FROM writer ";
+//$query .= "WHERE visible = 1 ";
+//$query .= "ORDER BY position ASC";
+$result = mysqli_query($dbconn, $query);
+  if(!$result){
+    die("Databases failed");
+  }
+?>
 
   <body>
     <header>
@@ -8,6 +20,13 @@
     </header>
     <article>
       <section class="main">
+          <?php
+          while($raw = mysqli_fetch_row($result)){
+              var_dump($raw);
+             //echo "<hr />";
+          }
+          ?>
+
 
       </section>
       <aside>
@@ -16,4 +35,7 @@
         </nav>
       </aside>
     </article>
+     <?php
+      mysqli_free_result($result);
+      ?>
 <?php include("../include/layout/footer.php"); ?>
