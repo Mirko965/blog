7cff85732a1f66f6c4145924405a498ce611d374
Mirko965 <mirkojelic.jelic@gmail.com>
2016-04-29 15:49:50 +0200

    Treba biti pazljiv unutar LOOP-a gde se stavljaju tagovi i mysqli_free_result

--- Public/main_page.php
+++ Public/main_page.php
@@ -14,49 +14,52 @@
       </section>
       <aside>
         <nav>
-<?php
-    //PERFORM DATABASES QUERY
-    $query  = "SELECT * " ;
-    $query .= "FROM writer ";
-    $query .= "WHERE visible = 1 ";
-    $query .= "ORDER BY position ASC";
-    $result = mysqli_query($dbconn, $query);
-      if(!$result){
-        die("Databases failed");
-      }
-?>
-        <ul>
-          <?php  while($writer_raw = mysqli_fetch_assoc($result)){ ?>
-             <li>
-                 <ul>
-                 <a href="main_page.php?writer="><?php echo $writer_raw["writer_name"]; ?></a>
-            <?php
-             $query  = "SELECT * " ;
-             $query .= "FROM text ";
-             $query .= "WHERE visible = 1 ";
-             $query .= "AND writer_id = {$writer_raw["id"]} ";
-             $query .= "ORDER BY position ASC";
-             $result_text = mysqli_query($dbconn, $query);
-               if(!$result_text){
-                 die("Databases failed");
-               }
-            ?>
-              <?php  while($raw_text = mysqli_fetch_assoc($result_text)){ ?>
-              <li><a href="main_page.php?text="><?php echo $raw_text["headline"]; ?></a></li>
-              <?php
+        <ul class="writer_nav">
+         <?php
+            //PERFORM DATABASES QUERY
+            $query  = "SELECT * " ;
+            $query .= "FROM writer ";
+            $query .= "WHERE visible = 1 ";
+            $query .= "ORDER BY position ASC";
+            $result = mysqli_query($dbconn, $query);
+              if(!$result){
+                die("Databases failed");
               }
-              ?>
-            <?php
-            }
+         ?>
+            <?php  while($writer_raw = mysqli_fetch_assoc($result)){ ?>
+            <li>
+               <a href="main_page.php?writer="><?php echo $writer_raw["writer_name"]; ?></a>
+               <?php
+                $query  = "SELECT * " ;
+                $query .= "FROM text ";
+                $query .= "WHERE visible = 1 ";
+                $query .= "AND writer_id = {$writer_raw["id"]} ";
+                $query .= "ORDER BY position ASC";
+                $result_text = mysqli_query($dbconn, $query);
+                  if(!$result_text){
+                    die("Databases failed");
+                  }
             ?>
-
-
+                <ul class="text_nav">
+                  <?php  while($raw_text = mysqli_fetch_assoc($result_text)){ ?>
+                <li>
+                    <a href="main_page.php?text="><?php echo $raw_text["headline"]; ?></a>
+                </li>
+                <?php
+                }
+                ?>
+                <?php
+                mysqli_free_result($result_text);
+                ?>
+                </ul>
             </li>
-            </ul>
             <?php
-             mysqli_free_result($result_text);
+              }
             ?>
-            </ul>
+            <?php
+            mysqli_free_result($result);
+            ?>
+        </ul>
         </nav>
       </aside>
     </article>
