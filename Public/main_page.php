80b2eaf3bbf391892bfe8e41900a5deb5128d19b
Mirko965 <mirkojelic.jelic@gmail.com>
2016-05-01 23:22:35 +0200

    "<li>  " GRESKA SVE VREME JE BILA U RAZMAKU "

--- Public/main_page.php
+++ Public/main_page.php
@@ -24,17 +24,37 @@ if(isset($_GET["writer"])){
          $result_select_writer = find_all_writer();
         ?>
             <ul class="writer">
-            <?php while($writer_raw = mysqli_fetch_assoc($result_select_writer)){ ?>
-            <li class="selected">
-                <a href="main_page.php?writer=<?php echo $writer_raw["id"]; ?>" ?><?php echo $writer_raw["writer_name"]; ?></a>
+            <?php
+            while($writer_raw = mysqli_fetch_assoc($result_select_writer)){
+            ?>
+                   <?php
+                   echo "<li ";
+                    if($writer_raw["id"] == $selected_writer_id){
+                   echo "class=\"selected\"";
+                    }
+                   echo ">";
+                   ?>
+                <a href="main_page.php?writer=<?php echo $writer_raw["id"]; ?>">
+                <?php echo $writer_raw["writer_name"]; ?></a>
                 <ul class="text">
                     <?php
-                      $result_select_text = find_text_by_writer_id($writer_raw["id"]);
+                    $result_select_text = find_text_by_writer_id($writer_raw["id"]);
                     ?>
-                    <?php while($text_raw = mysqli_fetch_assoc($result_select_text)) { ?>
-                    <li><a href="main_page.php?text=<?php echo $text_raw["id"];?>"><?php echo $text_raw["headline"]; ?></a></li>
+                      <?php
+                      while($text_raw = mysqli_fetch_assoc($result_select_text)) {
+                      ?>
+                     <?php
+                     echo "<li ";
+                      if($text_raw["id"] == $selected_text_id){
+                     echo "class=\"selected\"";
+                      }
+                     echo ">";
+                     ?>
+                    <a href="main_page.php?text=<?php echo $text_raw["id"];?>">
+                    <?php echo $text_raw["headline"]; ?></a>
+                    </li>
                     <?php
-                        }
+                    }
                     ?>
                     <?php mysqli_free_result($result_select_text); ?>
                 </ul>
