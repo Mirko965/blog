8470b8815f85785d189962d1a76857df0db9a2c3
Mirko965 <mirkojelic.jelic@gmail.com>
2016-05-01 22:37:30 +0200

    dosao sam do istog mesta gde sam sva izbrisao

--- Include/function.php
+++ Include/function.php
@@ -1,1 +1,31 @@
 <?php
+function confirm_query($result_set){
+    if(!$result_set){
+    die("Databases failed");
+    }
+}
+
+function find_all_writer(){
+    global $dbconn;
+
+    $query_select_writer  = "SELECT * " ;
+    $query_select_writer .= "FROM writer ";
+    $query_select_writer .= "WHERE visible = 1 ";
+    $query_select_writer .= "ORDER BY position ASC";
+    $result_select_writer = mysqli_query($dbconn, $query_select_writer);
+    confirm_query($result_select_writer);
+    return($result_select_writer);
+}
+
+function find_text_by_writer_id($writer_raw_id){
+    global $dbconn;
+
+    $query_select_text  = "SELECT * " ;
+    $query_select_text .= "FROM text " ;
+    $query_select_text .= "WHERE visible = 1 " ;
+    $query_select_text .= "AND writer_id = {$writer_raw_id} " ;
+    $query_select_text .= "ORDER BY position ASC";
+    $result_select_text = mysqli_query($dbconn, $query_select_text);
+    confirm_query($result_select_text);
+    return($result_select_text);
+}
--- Public/main_page.php
+++ Public/main_page.php
@@ -2,14 +2,48 @@
 <?php require_once("../include/function.php"); ?>
 <?php include("../include/layout/header.php"); ?>
 
-
+<?php
+if(isset($_GET["writer"])){
+    $selected_writer_id = $_GET["writer"];
+    $selected_text_id = null;
+}elseif(isset($_GET["text"])){
+    $selected_text_id = $_GET["text"];
+    $selected_writer_id = null;
+}else{
+    $selected_writer_id = null;
+    $selected_text_id = null;
+}
+?>
     <article class="main">
       <section class="content">
 
       </section>
       <aside role="navigation" id="navigation">
         <nav>
-
+        <?php
+         $result_select_writer = find_all_writer();
+        ?>
+            <ul class="writer">
+            <?php while($writer_raw = mysqli_fetch_assoc($result_select_writer)){ ?>
+            <li>
+                <a href="main_page.php?writer=<?php echo $writer_raw["id"]; ?>" ?><?php echo $writer_raw["writer_name"]; ?></a>
+                <ul class="text">
+                    <?php
+                      $result_select_text = find_text_by_writer_id($writer_raw["id"]);
+                    ?>
+                    <?php while($text_raw = mysqli_fetch_assoc($result_select_text)) { ?>
+                    <li><a href="main_page.php?text=<?php echo $text_raw["id"];?>"><?php echo $text_raw["headline"]; ?></a></li>
+                    <?php
+                        }
+                    ?>
+                    <?php mysqli_free_result($result_select_text); ?>
+                </ul>
+            </li>
+            <?php
+            }
+            ?>
+            <?php mysqli_free_result($result_select_writer); ?>
+            </ul>
         </nav>
       </aside>
     </article>
