6ed332f2142a4c6ec056af838fcaf722caf992d9
Mirko965 <mirkojelic.jelic@gmail.com>
2016-05-04 11:07:47 +0200

    refactoring find selected page

--- Public/main_page.php
+++ Public/main_page.php
@@ -1,31 +1,17 @@
 <?php require_once("../include/db_connection.php"); ?>
 <?php require_once("../include/function.php"); ?>
 <?php include("../include/layout/header.php"); ?>
-
-<?php
-if(isset($_GET["writer"])){
-    $selected_writer_id = $_GET["writer"];
-    $selected_text_id = null;
-}elseif(isset($_GET["text"])){
-    $selected_text_id = $_GET["text"];
-    $selected_writer_id = null;
-}else{
-    $selected_writer_id = null;
-    $selected_text_id = null;
-}
-?>
+<?php find_selected_text(); ?>
     <article class="main">
         <section class="content">
 
-            <?php if($selected_writer_id){ ?>
+            <?php if($current_writer){ ?>
             <h2>Menage writer</h2>
-            <?php   $current_writer = find_writer_by_id($selected_writer_id); ?>
             <p>Writer Name :
             <?php   echo $current_writer["writer_name"];?>
             </p>
-            <?php }elseif($selected_text_id){ ?>
+            <?php }elseif($current_text){ ?>
             <h2>Menage Page</h2>
-            <?php $current_text = find_text_by_id($selected_text_id) ?>
             <P>Headline :
             <?php   echo $current_text["headline"]; ?>
             </P>
@@ -38,7 +24,7 @@ if(isset($_GET["writer"])){
           <aside role="navigation" id="navigation">
             <nav>
             <?php
-                echo navigation($selected_writer_id,$selected_text_id)
+                echo navigation($current_writer,$current_text)
              ?>
             </nav>
           </aside>
