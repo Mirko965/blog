75208083ef9a5ba42bc56abe1d3f559094cba568
Mirko965 <mirkojelic.jelic@gmail.com>
2016-05-05 16:13:25 +0200

    izmene zbog greske

--- Public/create_new_writer.php
+++ Public/create_new_writer.php
@@ -3,28 +3,25 @@
 
 <?php
 if (isset($_POST['submit'])) {
-   	$writer_name = $_POST['writer_name'];
-	$position =  $_POST['position'];
-	$visible =  $_POST['visible'];
-
-	$writer_name = mysqli_real_escape_string($db_connection, $writer_name);
+	$writer_name = mysql_prep($_POST["writer_name"]);
+	$position = (int) $_POST["position"];
+	$visible = (int) $_POST["visible"];
 
 	$query  = "INSERT INTO writer (";
 	$query .= "  writer_name, position, visible";
 	$query .= ") VALUES (";
 	$query .= "  '{$writer_name}', {$position}, {$visible}";
 	$query .= ")";
-
-	$result = mysqli_query($db_connection, $query);
+	$result = mysqli_query($dbconn, $query);
 
 	if ($result) {
-		redirect_to("main_page.php");
         $message = "Writer created";
+		redirect_to("main_page.php");
 	} else {
 		 $message = "Writer creation failed";
-		die("Database query failed. " . mysqli_error($db_connection));
+		redirect_to("new_writer.php");
 	}
-} else {
+    } else {
     redirect_to("new_writer.php");
     }
 ?>
