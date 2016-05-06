a75037a8193c2ddbdac4d0e209487b1a30981886
Mirko965 <mirkojelic.jelic@gmail.com>
2016-05-06 22:28:19 +0200

    add required in input form

--- Public/new_writer.php
+++ Public/new_writer.php
@@ -8,10 +8,10 @@
         <?php echo message(); ?>
         <form action="create_new_writer.php" method="post">
             <p>Writer name:
-                <input type="text" name="writer_name" value="" >
+                <input type="text" name="writer_name" value="" required >
             </p>
             <p>Position:
-                <select name="position">
+                <select name="position" required>
                     <?php
                     $writer_number = find_all_writer();
                     $writer_count = mysqli_num_rows($writer_number);
@@ -22,8 +22,8 @@
                 </select>
             </p>
             <p>Visible:
-                <input type="radio" name="visible" value="1" />Yes
-                <input type="radio" name="visible" value="0" />No
+                <input type="radio" name="visible" value="1" required />Yes
+                <input type="radio" name="visible" value="0" required />No
             </p>
             <input type="submit" name="submit" value="create new writer" />
         </form>
