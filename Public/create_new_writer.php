7a31717f0966b924af8636f48addd77454c428d8
Mirko965 <mirkojelic.jelic@gmail.com>
2016-05-07 10:26:12 +0200

    in new_writer add fields for validation(required)

--- Public/create_new_writer.php
+++ Public/create_new_writer.php
@@ -1,9 +1,22 @@
 <?php require_once("../include/session.php"); ?>
 <?php require_once("../include/db_connection.php"); ?>
 <?php require_once("../include/function.php"); ?>
+<?php require_once("../include/validation_function.php"); ?>
 
 <?php
 if (isset($_POST['submit'])) {
+
+    $required_fields = array("writer_name","position","visible");
+    validate_presences($required_fields);
+
+    $fields_with_max_lengths = array("writer_name" => 30);
+    validate_max_lengths($fields_with_max_lengths);
+
+    if(!empty($errors)){
+        $_SESSION["errors"] = $errors;
+        redirect_to("new_writer.php");
+    }
+
 	$writer_name = mysql_prep($_POST["writer_name"]);
 	$position = (int) $_POST["position"];
 	$visible = (int) $_POST["visible"];
