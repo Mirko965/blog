53710d0fb4deaf1e713c6cfe8448149cecbf931c
Mirko965 <mirkojelic.jelic@gmail.com>
2016-05-22 15:12:04 +0200

    mora da se doda true,posle

--- Public/index.php
+++ Public/index.php
@@ -4,7 +4,7 @@
 
 <?php $layout_context = "public"; ?>
 <?php include("../include/layout/header.php"); ?>
-<?php find_selected_text(); ?>
+<?php find_selected_text(true); ?>
     <article class="main">
         <section class="content">
             <?php if($current_text){ ?>
