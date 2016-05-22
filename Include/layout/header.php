244647eeccea6133c6aca4608f1f225b67ed7ca8
Mirko965 <mirkojelic.jelic@gmail.com>
2016-05-22 13:22:54 +0200

    nista

--- Include/layout/header.php
+++ Include/layout/header.php
@@ -8,12 +8,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Blog <?php if($layout_context == "admin"){echo "Admin";}?></title>
+    <title>Blog <?php if($layout_context == "admin"){echo "-Admin";}?></title>
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
     <header id="header">
-      <h1>My Blog <?php if($layout_context == "admin"){echo "Admin";}?></h1>
+      <h1>My Blog <?php if($layout_context == "admin"){echo "-Admin";}?></h1>
       <img src="images/elephpant.png">
       <h2>PHP & MYSQL</h1>
     </header>
