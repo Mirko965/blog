8665dba85a1065c9a97be794c20f8e523c37be2b
Mirko965 <mirkojelic.jelic@gmail.com>
2016-05-22 15:17:49 +0200

    sitno

--- Public/index.php
+++ Public/index.php
@@ -8,14 +8,14 @@
     <article class="main">
         <section class="content">
             <?php if($current_text){ ?>
-            <div class="show_writer">
+
              <h2><?php echo $current_text["headline"];?></h2>
                 <?php echo $current_text["content"];?>
                 <?php }else{ ?>
                 <!--<h2>Menage content</h2>-->
                 <h3>Welcome</h3>
                 <?php } ?>
-            </div>
+
         </section>
           <aside role="navigation" id="navigation">
             <nav>
