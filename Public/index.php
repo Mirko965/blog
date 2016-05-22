d9b50655b564f33b6dad4370b13a036dd4011ea8
Mirko965 <mirkojelic.jelic@gmail.com>
2016-05-22 13:09:32 +0200

    sitno

--- Public/index.php
+++ Public/index.php
@@ -5,25 +5,15 @@
 <?php find_selected_text(); ?>
     <article class="main">
         <section class="content">
-            <?php
-            echo message();
-            ?>
-            <?php if($current_writer){ ?>
-
-                <div class="show_writer">
-                 <h2><?php echo $current_writer["writer_name"];?></h2>
-                </div>
-
-
-                    <?php }elseif($current_text){ ?>
-                    <h3><?php echo $current_text["headline"];?></h3>
-                        <?php echo $current_text["content"];?>
-                    <?php }else{ ?>
-                    <!--<h2>Menage content</h2>-->
-                    <p> Please select writer or text!</p>
-                    <?php } ?>
-
-
+            <?php if($current_text){ ?>
+            <div class="show_writer">
+             <h2><?php echo $current_text["headline"];?></h2>
+                <?php echo $current_text["content"];?>
+                <?php }else{ ?>
+                <!--<h2>Menage content</h2>-->
+                <h3>Welcome</h3>
+                <?php } ?>
+            </div>
         </section>
           <aside role="navigation" id="navigation">
             <nav>
