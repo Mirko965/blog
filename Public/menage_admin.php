b560bd8f261a7fe0f959d533aa69067c2e7e1d58
Mirko965 <mirkojelic.jelic@gmail.com>
2016-05-29 14:46:46 +0200

    nastavak

--- Public/menage_admin.php
+++ Public/menage_admin.php
@@ -14,12 +14,21 @@ $admin_set = find_all_admins();
             echo message();
             ?>
             <h2>Menage Admin</h2>
-            <?php
-            while($admin = mysqli_fetch_assoc($admin_set)){
-                echo $admin["username"];
-            }
-            ?>
-
+            <table>
+                <tr>
+                    <th>Username</th>
+                    <th colspan="2">Action</th>
+                </tr>
+                  <?php while($admin = mysqli_fetch_assoc($admin_set)){ ?>
+                <tr>
+                    <td> <?php  echo $admin["username"]; ?></td>
+                    <td> <a href="edit_admin.php?id=<?php echo $admin["id"]; ?>">Edit </a> </td>
+                    <td> <a href="delete_admin.php?id=<?php echo $admin["id"]; ?>">Delete </a> </td>
+                </tr>
+                  <?php } ?>
+            </table>
+            <br>
+            <a href="new_admin.php">Add a new Admin</a>
         </section>
           <aside role="navigation" id="navigation">
             <nav>
@@ -27,3 +36,4 @@ $admin_set = find_all_admins();
             </nav>
           </aside>
     </article>
+<?php include("../include/layout/footer.php"); ?>
